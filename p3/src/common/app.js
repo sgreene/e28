6c922e5f7d5d0caca11e3cad5d70c8aecf8197b0
sgreene <simeon.greene@gmail.com>
2021-05-11 00:42:19 -0400

    Fixed bug in app.js (wrong env variable for production)

--- p3/src/common/app.js
+++ p3/src/common/app.js
@@ -1,6 +1,6 @@
 export const axios = require('axios').create(
     {
-        baseURL: process.env.VUE_APP_API_URL ?? 'http://e28api.trinigooner.loc/',
+        baseURL: process.env.VUE_APP_APIURL ?? 'http://e28api.trinigooner.loc/',
         responseType: 'json',
         withCredentials: true
     }
